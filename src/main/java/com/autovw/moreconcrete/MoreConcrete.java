89cac8ccd13c48daffa3ed0988481da57cd7e524
Autovw <68228006+Autovw@users.noreply.github.com>
2022-03-03 12:16:19 +0100

    ⚡ Moved to slf4j

--- src/main/java/com/autovw/moreconcrete/MoreConcrete.java
+++ src/main/java/com/autovw/moreconcrete/MoreConcrete.java
@@ -1,20 +1,20 @@
 package com.autovw.moreconcrete;
 
 import com.autovw.moreconcrete.core.ModBlocks;
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 /**
  * Author: Autovw
  */
 @Mod(MoreConcrete.MODID)
 public class MoreConcrete {
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
     public static final String MODID = "moreconcrete";
 
     public MoreConcrete() {
