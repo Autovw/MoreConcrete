22ba78eb88df28b429e91ce582d8c9e0d9c14bdc
Autovw <68228006+Autovw@users.noreply.github.com>
2023-09-17 12:21:53 +0200

    🐛 Fix blocks not appearing in the creative tabs

--- src/main/java/com/autovw/moreconcrete/MoreConcreteTab.java
+++ src/main/java/com/autovw/moreconcrete/MoreConcreteTab.java
@@ -20,14 +20,14 @@ public class MoreConcreteTab {
 
     @SubscribeEvent
     public static void onCreativeModeTabEvent(final BuildCreativeModeTabContentsEvent event) {
-        if (event.getTab().equals(CreativeModeTabs.COLORED_BLOCKS)) {
+        if (event.getTabKey().equals(CreativeModeTabs.COLORED_BLOCKS)) {
             ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(predicate -> {
                 return predicate instanceof SlabBlock || predicate instanceof StairBlock ||
                         predicate instanceof WallBlock || predicate instanceof FenceBlock;
             }).forEach(event::accept);
         }
 
-        if (event.getTab().equals(CreativeModeTabs.REDSTONE_BLOCKS)) {
+        if (event.getTabKey().equals(CreativeModeTabs.REDSTONE_BLOCKS)) {
             ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(predicate -> {
                 return predicate instanceof LeverBlock || predicate instanceof PressurePlateBlock ||
                         predicate instanceof FenceGateBlock || predicate instanceof ButtonBlock;
