2193f3f7df3449120d426b57d2d8f7b5876e34e4
Autovw <68228006+Autovw@users.noreply.github.com>
2025-03-27 17:13:31 +0100

    ✨ Dedicated concrete creative tab and improved sorting

--- forge/src/main/java/com/autovw/moreconcrete/forge/MoreConcreteTab.java
+++ forge/src/main/java/com/autovw/moreconcrete/forge/MoreConcreteTab.java
@@ -1,11 +1,17 @@
 package com.autovw.moreconcrete.forge;
 
+import com.autovw.moreconcrete.common.MoreConcrete;
+import com.autovw.moreconcrete.common.common.ColourUtil;
 import com.autovw.moreconcrete.forge.core.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.*;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 import static com.autovw.moreconcrete.common.MoreConcrete.MOD_ID;
@@ -16,6 +22,12 @@ import static com.autovw.moreconcrete.common.MoreConcrete.MOD_ID;
 @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class MoreConcreteTab
 {
+    static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MoreConcrete.MOD_ID);
+    static final RegistryObject<CreativeModeTab> TAB = TABS.register("tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup." + MoreConcrete.MOD_ID + ".tab"))
+            .icon(ModBlocks.LIGHT_BLUE_CONCRETE_STAIRS.get().asItem()::getDefaultInstance)
+            .build());
+
     private MoreConcreteTab()
     {
     }
@@ -25,18 +37,23 @@ public final class MoreConcreteTab
     {
         if (event.getTabKey().equals(CreativeModeTabs.COLORED_BLOCKS))
         {
-            ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(predicate -> {
+            ColourUtil.sortTab(ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(predicate -> {
                 return predicate instanceof SlabBlock || predicate instanceof StairBlock ||
                         predicate instanceof WallBlock || predicate instanceof FenceBlock;
-            }).forEach(event::accept);
+            })).forEach(event::accept);
         }
 
         if (event.getTabKey().equals(CreativeModeTabs.REDSTONE_BLOCKS))
         {
-            ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(predicate -> {
+            ColourUtil.sortTab(ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).filter(predicate -> {
                 return predicate instanceof LeverBlock || predicate instanceof PressurePlateBlock ||
                         predicate instanceof FenceGateBlock || predicate instanceof ButtonBlock;
-            }).forEach(event::accept);
+            })).forEach(event::accept);
+        }
+
+        if (event.getTab().equals(TAB.get()))
+        {
+            ColourUtil.sortConcreteTab(ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)).forEach(event::accept);
         }
     }
 }
--- neoforge/src/main/java/com/autovw/moreconcrete/neoforge/MoreConcreteTab.java
+++ neoforge/src/main/java/com/autovw/moreconcrete/neoforge/MoreConcreteTab.java
@@ -1,13 +1,18 @@
 package com.autovw.moreconcrete.neoforge;
 
 import com.autovw.moreconcrete.common.MoreConcrete;
+import com.autovw.moreconcrete.common.common.ColourUtil;
 import com.autovw.moreconcrete.neoforge.core.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.*;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 /**
  * @author Autovw
@@ -15,6 +20,12 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 @EventBusSubscriber(modid = MoreConcrete.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public final class MoreConcreteTab
 {
+    static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MoreConcrete.MOD_ID);
+    static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB = TABS.register("tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup." + MoreConcrete.MOD_ID + ".tab"))
+            .icon(ModBlocks.LIGHT_BLUE_CONCRETE_STAIRS::toStack)
+            .build());
+
     private MoreConcreteTab()
     {
     }
@@ -24,18 +35,23 @@ public final class MoreConcreteTab
     {
         if (event.getTabKey().equals(CreativeModeTabs.COLORED_BLOCKS))
         {
-            ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).filter(predicate -> {
+            ColourUtil.sortTab(ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).filter(predicate -> {
                 return predicate instanceof SlabBlock || predicate instanceof StairBlock ||
                         predicate instanceof WallBlock || predicate instanceof FenceBlock;
-            }).forEach(event::accept);
+            })).forEach(event::accept);
         }
 
         if (event.getTabKey().equals(CreativeModeTabs.REDSTONE_BLOCKS))
         {
-            ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).filter(predicate -> {
+            ColourUtil.sortTab(ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).filter(predicate -> {
                 return predicate instanceof LeverBlock || predicate instanceof PressurePlateBlock ||
                         predicate instanceof FenceGateBlock || predicate instanceof ButtonBlock;
-            }).forEach(event::accept);
+            })).forEach(event::accept);
+        }
+
+        if (event.getTab().equals(TAB.get()))
+        {
+            ColourUtil.sortConcreteTab(ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get)).forEach(event::accept);
         }
     }
 }
